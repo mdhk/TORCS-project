6282ab6a8884da3d4515da766bef4a70bc47f93f
mdhk <mariannedhk@gmail.com>
2017-11-10 11:11:55 +0100

    added training data + read_data function (rnn.py)

---
+++ torcs-server/torcs-client/rnn.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+def read_data(*csv_filepaths):
+	"""
+	Reads data from csv files, returns X and Y as numpy matrices.
+	"""
+	X_data = []
+	Y_data = []
+	for csv_file in csv_filepaths:
+		data = pd.read_csv(csv_file, sep=',', header=0)
+		Y_data.append(np.matrix(data.values)[:,:3])
+		X_data.append(np.matrix(data.values)[:,3:])
+	Y = np.concatenate(Y_data, axis=0)
+	X = np.concatenate(X_data, axis=0)
+	return X, Y
+
+X, Y = read_data('train_data/aalborg.csv', 'train_data/alpine-1.csv', 'train_data/f-speedway.csv')
+print(X.shape, Y.shape)
+
+
+		
