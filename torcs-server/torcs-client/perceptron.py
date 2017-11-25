ed3526f75190d70f94828b2ea19a5103ce6cae3c
Marianne <mariannedhk@gmail.com>
2017-11-25 19:42:22 +0100

    added multilayer perceptron

--- torcs-server/torcs-client/perceptron.py
+++ torcs-server/torcs-client/perceptron.py
@@ -4,7 +4,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-class ExampleNN(nn.Module):
+X = train_data.data_tensor
+Y = train_data.target_tensor
+N_in = len(X[1])
+N_out = len(Y[1])
+N_hidden = 30
+N_epochs = 500
+
+class Perceptron(nn.Module):
 
     def __init__(self):
         super(ExampleNN, self).__init__()
@@ -14,37 +21,39 @@ class ExampleNN(nn.Module):
 
     def forward(self, x):
         x = self.linear(x)
-        x = F.sigmoid(x)  # output values are squashed between 0 and 1
+        x = F.sigmoid(x)
         return x
 
-net = ExampleNN()
+class MLP(nn.Module):
+            def __init__(self):
+                super(MLP, self).__init__()
+                self.hidden = nn.Linear(N_in, N_hidden)
+                self.hidden2 = nn.Linear(N_hidden, N_hidden)
+                self.out   = nn.Linear(N_hidden, N_out)
 
-x = Variable(train_data.data_tensor)
+            def forward(self, x):
+                x = F.relu(self.hidden(x))
+                x = F.tanh(self.hidden2(x))
+                x = self.out(x)
+                return x
+
+net = MLP()
+x = Variable(X)
 out = net(x)
-target = Variable(train_data.target_tensor)
+target = Variable(Y)
 
 criterion = nn.MSELoss()
-loss = criterion(out, target)
 
-print("output:", out)
-print("loss:", loss)
+for epoch in range(N_epochs):
+    loss = criterion(out, target)
+    print('epoch', epoch + 1, ', loss:', loss)
+    net.zero_grad()  # reset gradients
+    loss.backward()  # compute gradients
+    # update weights
+    learning_rate = 0.01
+    for f in net.parameters():
+        # for each parameter, take a small step in the opposite dir of the gradient
+        # sub_ substracts in-place
+        f.data.sub_(f.grad.data * learning_rate)
 
-net.zero_grad()  # reset gradients
-loss.backward()  # compute gradients
-
-# update weights
-learning_rate = 0.5
-for f in net.parameters():
-    # for each parameter, take a small step in the opposite dir of the gradient
-    # sub_ substracts in-place
-    f.data.sub_(f.grad.data * learning_rate)
-
-new_out = net(x)
-new_loss = criterion(new_out, target)
-
-print("target:", target)
-print("out:", out)
-print("new out (should be closer to target):", new_out)
-
-print("\nloss:", loss)
-print("new loss (should be lower):", new_loss)
+    out = net(x)
