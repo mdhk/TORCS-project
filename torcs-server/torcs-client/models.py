2de29a747f13d26786aaecd69ee3d84d6f99829a
Marianne <mariannedhk@gmail.com>
2017-11-26 14:45:26 +0100

    speed is now learned, mlp drives car through the track like a grandma

--- torcs-server/torcs-client/models.py
+++ torcs-server/torcs-client/models.py
@@ -9,7 +9,7 @@ X = train_data.data_tensor
 Y = train_data.target_tensor
 N_in = len(X[1])
 N_out = len(Y[1])
-N_hidden = 30
+N_hidden = 95
 
 class Perceptron(nn.Module):
 
@@ -25,42 +25,73 @@ class Perceptron(nn.Module):
         return x
 
 class MLP(nn.Module):
-            def __init__(self):
-                super(MLP, self).__init__()
-                self.hidden = nn.Linear(N_in, N_hidden)
-                self.hidden2 = nn.Linear(N_hidden, N_hidden)
-                self.out   = nn.Linear(N_hidden, N_out)
+        def __init__(self):
+            super(MLP, self).__init__()
+            self.hidden = nn.Linear(N_in, N_hidden)
+            self.hidden2 = nn.Linear(N_hidden, N_hidden)
+            self.out   = nn.Linear(N_hidden, N_out)
 
-            def forward(self, x):
-                x = F.relu(self.hidden(x))
-                x = F.tanh(self.hidden2(x))
-                x = self.out(x)
-                return x
+        def forward(self, x):
+            x = F.relu(self.hidden(x))
+            x = F.tanh(self.hidden2(x))
+            x = self.out(x)
+            return x
 
-            def predict(self, x):
-                x = Variable(torch.FloatTensor(np.asarray(x)))
-                y = self.forward(x).data.numpy()[0]
-                return y
+        def predict(self, x):
+            x = Variable(torch.FloatTensor(np.asarray(x)))
+            y = self.forward(x).data.numpy()[0]
+            return y
+
+## TODO RNN
+# class RNN(nn.Module):
+#     def __init__(self, input, hidden_size, output_size):
+#         super(RNN, self).__init__()
+#
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#
+#         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+#         self.i2o = nn.Linear(input_size + hidden_size, output_size)
+#         self.softmax = nn.LogSoftmax()
+#
+#     def forward(self, input, hidden):
+#         combined = torch.cat((input, hidden), 1)
+#         hidden = self.i2h(combined)
+#         output = self.i2o(combined)
+#         output = self.softmax(output)
+#         return output, hidden
+#
+#     def initHidden(self):
+#         return Variable(torch.zeros(1, self.hidden_size))
+#
+#     def lineToTensor(line):
+#         tensor = torch.zeros(len(line), 1, N_input)
+#         for li, letter in enumerate(line):
+#             tensor[li][0][letterToIndex(letter)] = 1
+#         return tensor
 
 net = MLP()
-net.load_state_dict(torch.load('models/mlp500'))
 
-### UNCOMMENT FOR TRAINING
+### UNCOMMENT FOR LOADING A TRAINED MODEL
+net.load_state_dict(torch.load('models/mlp100'))
+
+# UNCOMMENT FOR TRAINING
 # N_epochs = 100
-# net = MLP()
 # x = Variable(X)
 # out = net(x)
 # target = Variable(Y)
 #
+# ## choose a loss function, e.g. nn.MSELoss() or nn.NLLLoss()
 # criterion = nn.MSELoss()
 #
 # for epoch in range(N_epochs):
 #     loss = criterion(out, target)
-#     print('epoch', epoch + 1, ', loss:', loss)
+#     if epoch % 10 == 0:
+#         print('epoch', epoch + 1, ', loss:', loss)
 #     net.zero_grad()  # reset gradients
 #     loss.backward()  # compute gradients
 #     # update weights
-#     learning_rate = 0.01
+#     learning_rate = 0.02
 #     for f in net.parameters():
 #         # for each parameter, take a small step in the opposite dir of the gradient
 #         # sub_ substracts in-place
@@ -68,4 +99,4 @@ net.load_state_dict(torch.load('models/mlp500'))
 #
 #     out = net(x)
 #
-# torch.save(net.state_dict(), 'models/mlp500')
+# torch.save(net.state_dict(), 'models/mlp100')
--- torcs-server/torcs-client/my_driver.py
+++ torcs-server/torcs-client/my_driver.py
@@ -1,5 +1,5 @@
 from pytocl.driver import Driver
-from pytocl.car import State, Command
+from pytocl.car import State, Command, KMH_PER_MPS
 from models import net
 import numpy as np
 
@@ -7,27 +7,19 @@ class MyDriver(Driver):
     # Override the `drive` method to create your own driver
     ...
     def drive(self, carstate: State) -> Command:
-
         x = np.asarray([[carstate.speed_x, carstate.distance_from_center, carstate.angle,
              *carstate.distances_from_edge]])
         y = net.predict(x)
 
         command = Command()
 
-        command.accelerator = y[0]
-        command.brake = y[1]
-        command.speed = y[2]
+        self.steer(carstate, y[2], command)
 
-        if carstate.rpm > 7500:
-            command.gear = carstate.gear + 1
-
-        if carstate.rpm < 2500 and carstate.gear > 0:
-            command.gear = carstate.gear - 1
-
-        if carstate.gear < 1:
-            carstate.gear = 1
-
-        if not command.gear:
-            command.gear = carstate.gear
+        if (carstate.speed_x * KMH_PER_MPS) < 80 and carstate.distance_raced < 100:
+            self.accelerate(carstate, 100, command)
+        else:
+            self.accelerate(carstate, y[3], command)
+            command.accelerator = y[0]
+            command.brake = y[1]
 
         return command
