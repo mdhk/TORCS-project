fixed normalisation

torcs-server/torcs-client/computer_driver.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import pickleshare
 import networks as nw
 from train_mlp import net
-from load_data import mean_normalisation
 
 # network = "models/mlp101"
 # net = nn.Module.load_state_dict(torch.load(network))
@@ -61,16 +60,13 @@ class MyDriver(Driver):
 
         x = data
         y = net.predict(x)
-        print(x)
-        print(y)
 
         command = Command()
 
-
         self.steer(carstate, y[3], command)
-        # command.steering = y[3]
+        command.steering /= 10
 
-        # command.accelerator = y[0]
+        #command.accelerator = y[0]
 
         # ## Uncomment to disable brakes for more fun
         # if np.abs(y[1]) <= 0.03 or (carstate.speed_x * KMH_PER_MPS) < 90 or \

torcs-server/torcs-client/networks.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+import cloudpickle
+stats = cloudpickle.load(open('train_data/stats.pkl', 'rb'))
 
 """
 Geen uitvoerende code binnen dit bestand!
@@ -38,8 +40,10 @@ class neural_net(nn.Module):
         return x
 
     def predict(self, x):
-        x = Variable(torch.FloatTensor(np.asarray(x)))
+        normalised_x = (x - stats['means'])/stats['stds']
+        x = Variable(torch.FloatTensor(np.asarray(normalised_x)))
         y = self.forward(x).data.numpy()[0]
+        #print(normalised_x, '\n', y)
         return y
 
 
@@ -59,7 +63,7 @@ class MLP(neural_net):
 
         ## choose a loss function, e.g. nn.MSELoss() or nn.NLLLoss()
         criterion = nn.MSELoss()
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.02)
 
         for epoch in range(1, N_epochs + 1):
             optimizer.zero_grad()

torcs-server/torcs-client/train_mlp.py
@@ -1,4 +1,4 @@
-import dill
+import cloudpickle
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,16 +7,21 @@ from torch.autograd import Variable
 import numpy as np
 import networks as nw
 
-race_list = dill.load(open('train_data/race_list.pkl', 'rb'))
-X = torch.FloatTensor(np.concatenate([tensordataset.data_tensor.numpy() \
-    for tensordataset in race_list], axis=0))
+race_list = cloudpickle.load(open('train_data/race_list.pkl', 'rb'))
+data = np.concatenate([tensordataset.data_tensor.numpy() \
+        for tensordataset in race_list], axis=0)
+X = torch.FloatTensor(data)
 Y = torch.FloatTensor(np.concatenate([tensordataset.target_tensor.numpy() \
     for tensordataset in race_list], axis=0))
 train_data = TensorDataset(X, Y)
 net = nw.MLP(train_data.data_tensor, train_data.target_tensor, N_hidden=50)
 
-epochs = 501
-# network = "models/mlp" + str(epochs)
-# net.load(network)
+epochs = 50
+#network = "models/mlp" + str(epochs)
+#net.load(network)
 if __name__ == "__main__":
     net.train(epochs)
+    data_means = np.mean(data, axis=0)
+    data_stds = np.std(data, axis=0)
+    stats = {'means': data_means, 'stds': data_stds}
+    cloudpickle.dump(stats, open('train_data/stats.pkl', 'wb'))